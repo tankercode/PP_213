abe2ee725e567855ab3d3179b4c0d9d2704a6eb8
tankercode <tanker1609dd@gmail.com>
2024-07-09 19:10:50 +0700

    Add Cat class uncommented AppTest class

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloWorld");
+        
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloWorld");
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean1);
+        System.out.println(bean2 == bean3);
+    }
+}
